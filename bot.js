6e98dd04c667988fe3f6137d5ef047d51202f25e
victory-clan <46355716+victory-clan@users.noreply.github.com>
2019-02-03 06:39:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('ready', () => {
 
 client.on('ready', async() => {
 var server = "437236584329379841"; // ايدي السررفر
-var channel = "539853412821630977";//ايدي الروم
+var channel = "541460969683943434";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('احبكم باكو   :heart: , احبكم باكو  :heart: , احبكم باكو  :heart:')
     },305);
